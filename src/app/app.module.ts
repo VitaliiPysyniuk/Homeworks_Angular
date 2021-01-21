27a6bb148822c26deb9a43dd7424993baae18540
Vitalii <Viitaliy2112@gmail.com>
2021-01-22 01:32:27 +0200

    cleaned_master_branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
--- src/database/carDatabase.ts
+++
@@ -1,8 +0,0 @@
-export let cars = [
-  {brand: 'Tesla', model: 'Model S', price: 60000},
-  {brand: 'Ford', model: 'F-150', price: 48500},
-  {brand: 'Zaz', model: 'Sens', price: 3000},
-  {brand: 'Renault', model: 'Laguna', price: 8500},
-  {brand: 'Volkswagen', model: 'B6', price: 7000},
-  {brand: 'Tesla', model: 'Model X', price: 100000},
-];
